9b37e0b446606d326c16b80534a6b3a1ea7078a4
LunarSalameh <92386389+LunarSalameh@users.noreply.github.com>
2023-05-21 20:26:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/librarycatalog/ChildrenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.librarycatalog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChildrenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_children)
+    }
+}
